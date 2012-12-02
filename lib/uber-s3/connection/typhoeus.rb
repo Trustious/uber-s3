b997846bc1db24574e9492076de54624e31f8c4a
Saher El-Neklawy <saher@saher-laptop.(none)>
2012-12-02 13:36:59 +0200

    Adding a second retry for typhoeus handler

--- lib/uber-s3/connection/typhoeus.rb
+++ lib/uber-s3/connection/typhoeus.rb
@@ -3,34 +3,50 @@ require 'typhoeus'
 HYDRA = ::Typhoeus::Hydra.new(:max_concurrency => 100)
 
 module UberS3::Connection
-  class Typhoeus < Adapter
-    
-    def request(verb, url, headers={}, body=nil)
-      # TODO: see extra steps done in NET::HTTP adapter
-      params = {}
-      params[:headers] = headers
-      params[:method] = verb.to_sym
-      params[:body] = body if body
+    class Typhoeus < Adapter
+        def request(verb, url, headers={}, body=nil)
+            # TODO: see extra steps done in NET::HTTP adapter
+            params = {}
+            params[:headers] = headers
+            params[:method] = verb.to_sym
+            params[:body] = body if body
 
-      r = ::Typhoeus::Request.new(url, params)
-      r.on_complete do |response|
-          result = UberS3::Response.new({
-              :status => response.code,
-              :header => response.headers,
-              :body   => response.body,
-              :raw    => response
-          })
-          puts '#'*50, 'error handling request', url, params, result.inspect unless response.code < 400
-          result
-      end
-      HYDRA.queue(r)
-      UberS3::Response.new({
-          :status => 200,
-          :header => "",
-          :body   => "",
-          :raw    => ""
-      })
+            r = ::Typhoeus::Request.new(url, params)
+            r.on_complete do |response|
+                begin
+                    result = UberS3::Response.new({
+                        :status => response.code,
+                        :header => response.headers,
+                        :body   => response.body,
+                        :raw    => response
+                    })
+                rescue
+                    puts '#'*50, 'error handling request, will retry', url, params, result.inspect
+                    r2 = ::Typhoeus::Request.new(url, params)
+                    r2.on_complete do |response2|
+                        begin
+                            result = UberS3::Response.new({
+                                :status => response2.code,
+                                :header => response2.headers,
+                                :body   => response2.body,
+                                :raw    => response2
+                            })
+                        rescue
+                            puts '#'*50, 'error handling request gave up', url, params, result.inspect
+                        end
+                        result
+                    end
+                    HYDRA.queue(r2)
+                    result
+                end
+            end
+            HYDRA.queue(r)
+            UberS3::Response.new({
+                :status => 200,
+                :header => "",
+                :body   => "",
+                :raw    => ""
+            })
+        end
     end
-    
-  end
 end
